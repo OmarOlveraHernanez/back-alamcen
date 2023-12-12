7b2319b231b8c9422ce5937d896d181921e5ce75
omartiendas3b <aoo@tiendas3b.com>
2023-12-12 12:15:05 -0600

    add change

--- src/almacen_payment/dto/create-almacen_payment.dto.ts
+++ src/almacen_payment/dto/create-almacen_payment.dto.ts
@@ -1,1 +1,17 @@
-export class CreateAlmacenPaymentDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsString, ValidateNested } from "class-validator";
+import { AlmacenPaymentDto } from "./almacen-payment.dto";
+
+export class CreateAlmacenPaymentDto {
+
+    @ApiProperty()
+    @ValidateNested()
+    @Type(() => AlmacenPaymentDto)
+    resource: AlmacenPaymentDto;
+    
+
+    @ApiProperty()
+    @IsString()
+    almacen: string;
+}
